24c090ba998fbdbc2202a6c2eddbae0be802bd77
Siddiq Mansoory <siddiq@cmsminds.com>
2022-05-03 01:23:44 +0530

    Remove default page from Maintainance mode

--- sites/all/themes/___pressa/templates/block--title--title.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php if ($title): ?>
-    <h1 class="block-title"><?php print $title; ?></h1>
-  <?php if ($subtitle): ?>
-      <p class="block-subtitle"><?php print $subtitle; ?></p>
-  <?php endif; ?>
-    <hr/>
-<?php endif; ?>
--- sites/all/themes/___pressa/templates/shortcode/shortcode--piegraph.tpl.php
+++
@@ -1,2 +0,0 @@
-<div class="dexp-pie-chart" data-percent="<?php print $percent; ?>"></div>
-<h3 class="dexp-pie-chart-title"><?php print $title;?></h3>
